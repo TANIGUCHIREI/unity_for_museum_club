910f8a83cc6c5f2604edf56459817c65195a4464
DESKTOP-5TID4Q1\tanigutirei <taniguchirei0j@gmail.com>
2023-10-17 09:15:21 +0900

    add setting menu

--- Assets/Scenes/Scripts/ClientManager.cs
+++ Assets/Scenes/Scripts/ClientManager.cs
@@ -8,6 +8,7 @@ using WebSocketSharp.Net;
 using UnityEngine.UI;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+
 public class ClientManager : MonoBehaviour
 {
     //https://qiita.com/riyosy/items/5789ccdeee644b34a743
@@ -25,6 +26,8 @@ public class ClientManager : MonoBehaviour
     public string userinput_text;
     public bool _is_kansai_only;
 
+    public static string IPAdress = PlayerPrefs.GetString("IPAdress","localhost");
+    public static string Port = PlayerPrefs.GetString("Port", "8001");
 
     void Start()
     {
@@ -33,9 +36,7 @@ public class ClientManager : MonoBehaviour
         _is_kansai_only = gameObject.GetComponent<User_Input>()._is_kansai_only;
 
         //接続処理。接続先サーバと、ポート番号を指定する
-        ws = new WebSocket("ws://localhost:8001/");
-        ws.Connect();
-        OnSendPython();
+        Test_CoMwithPython();
         //送信ボタンが押されたときに実行する処理「SendText」を登録する
         //endButton.onClick.AddListener(SendText);
 
@@ -47,7 +48,24 @@ public class ClientManager : MonoBehaviour
 
     }
 
+    
+    public void Test_CoMwithPython()
+    {
+        try {
+            Debug.Log("try to connetct to " + "ws://" + IPAdress + ":" + Port + "/");
+            ws = new WebSocket("ws://" + IPAdress + ":" + Port + "/");
+            ws.Connect();
 
+            user_input_dict = new Dictionary<string, string> { { "TYPE", "COM_TEST" } };
+            string json = JsonConvert.SerializeObject(user_input_dict);
+            byte[] json_bytes = Encoding.UTF8.GetBytes(json);
+            ws.Send(json_bytes);
+        }catch (System.InvalidOperationException e)
+        {
+            Debug.Log("接続先がオープンされていません！");
+        }
+        
+    }
     public void OnSendPython()
     {
         string send_word = CreateInput();
@@ -72,9 +90,10 @@ public class ClientManager : MonoBehaviour
         return input;
     }
     public void SendToPython(string user_input)
+
     {
         //ws.Send(messageInput.text);
-        user_input_dict = new Dictionary<string, string> { { "user_input", user_input } };
+        user_input_dict = new Dictionary<string, string> { { "TYPE", "USER_INPUT" } ,{ "user_input", user_input } };
         string json = JsonConvert.SerializeObject(user_input_dict);
         byte[] json_bytes = Encoding.UTF8.GetBytes(json);
         ws.Send(json_bytes);
@@ -84,29 +103,35 @@ public class ClientManager : MonoBehaviour
     public void RecvFromPython(byte[] msg)
     {
         var recieved_words_data = Encoding.UTF8.GetString(msg);
-        Dictionary<string, object>  dictionary = JsonConvert.DeserializeObject<Dictionary<string, object>>(recieved_words_data);
+        Dictionary<string, object>  recv_dict = JsonConvert.DeserializeObject<Dictionary<string, object>>(recieved_words_data);
         //Debug.Log(dict["TYPE"]);
         //Debug.Log(recieved_words_data);
         //Debug.Log(dict["TYPE"]);
-        if((string) dictionary["TYPE"] == "QUERY")
+        string Recv_Type = (string)recv_dict["TYPE"];
+
+        if(Recv_Type == "QUERY")
         {
             //Debug.Log("クエリだよ");
             //Debug.Log(dictionary["QUERY"].GetType());
-            List<string> QUERY = (dictionary["QUERY"] as JArray).ToObject<List<string>>();
+            List<string> QUERY = (recv_dict["QUERY"] as JArray).ToObject<List<string>>();
             foreach (var word in QUERY)
             {
                 Debug.Log(word);
             }
             
-        }else if ((string) dictionary["TYPE"] == "ANSWER")
+        }else if (Recv_Type == "ANSWER")
         {
-            string prefecture = (string) dictionary["prefecture"];
-            string museum_name = (string)dictionary["museum_name"];
-            string exhibition_name = (string)dictionary["exhibition_name"];
-            string exhibition_reason = (string)dictionary["exhibition_reason"];
+            string prefecture = (string) recv_dict["prefecture"];
+            string museum_name = (string)recv_dict["museum_name"];
+            string exhibition_name = (string)recv_dict["exhibition_name"];
+            string exhibition_reason = (string)recv_dict["exhibition_reason"];
             Debug.Log(prefecture + " " + museum_name + " " + exhibition_name);
             Debug.Log(exhibition_reason);
 
+        }else if (Recv_Type == "COM_TEST")
+        {
+            string Response_from_Python = (string)recv_dict["RESPONSE"];
+            Debug.Log(Response_from_Python);
         }
     }
     //サーバの接続が切れたときのメッセージを、ChatTextに表示する
